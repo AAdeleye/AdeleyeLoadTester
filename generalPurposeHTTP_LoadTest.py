ae4d3f7e908e7d8cea57f91896af4485a563268e
Akanimoh <sanmi.adeleye@gmail.com>
2024-05-29 23:44:06 -0700

    Added csv file for multiple urls

--- generalPurposeHTTP_LoadTest.py
+++ generalPurposeHTTP_LoadTest.py
@@ -5,8 +5,8 @@ import time
 import asyncio
 import aiohttp
 from tqdm import tqdm
-
-
+import os
+import pandas as pd
 
 
 async def runURLRequest(url,session):
@@ -63,19 +63,35 @@ async def urlRequestStats(url,total_num_request,num_concurrent,qps):
 
     average_latency = totalLatency / total_num_request if total_num_request > 0 else 0
     error_rate = totalErrors / total_num_request if total_num_request > 0 else 0
+    print(f"Testing URL: {url}")
     print(f"The Avereage Error Rate is: {error_rate}")
     print(f"The Average Latency is: {average_latency:.2f} seconds")
+    print(f"This test was completed with Total request: {total_num_request}, Concurrent request: {num_concurrent}, with a QPS of: {qps}")
     
 
-def main(url,num_request,num_concurrent,qps):
-    asyncio.run(urlRequestStats(url,num_request,num_concurrent,qps))
-
-
+def main(url_or_csv,num_request,num_concurrent,qps):
+    if not url_or_csv.lower().endswith('.csv'):
+        asyncio.run(urlRequestStats(url_or_csv,num_request,num_concurrent,qps))
+    else:
+        if not os.path.isfile(url_or_csv):
+            print(f"Error: The file {url_or_csv} does not exist.")
+            exit()
+        try:
+            df = pd.read_csv(url_or_csv)
+        except Exception as e:
+            print(f"File was not able to be read. Please make sure file is in CSV format where urls are on one line seperated by commas.\n Error: {e} ")
+        
+        for url in df.columns:
+            if not validators.url(url):
+                print(f"{url} not valid. Please check link and be sure to include full https://... url.")
+            else:
+                asyncio.run(urlRequestStats(url,num_request,num_concurrent,qps))
 
 if __name__ == "__main__":
     # Parse URL(s) 
     parser = argparse.ArgumentParser(description='Arguments for Https Load tester')
     parser.add_argument('--url_link', type=str, help='Enter URL Link you want to test')
+    parser.add_argument('--url_csv',type=str, help='Enter a csv file with URL Links you want to test')
     parser.add_argument('--qps', type=int, default=0, help='Generate request at given fixed QPS. Plese note, if the --c flag is not set to zero, concurrent request may be sent at the fixed QPS.')
     parser.add_argument('--n',type=int, default=1000, help='total number of request to preform.')
     parser.add_argument('--c',type=int, default=100, help='number of concurrent request to preform untill total number of request is reached.')
@@ -85,14 +101,15 @@ if __name__ == "__main__":
     # 2. Accpet a csv file of Http Websites
     # 3. Save results to a txt file. 
 
-    if not parse_args.url_link:
-        print("Please enter a url link you want to test. Example: 'docker run fireworksai:app --url_link http:/www.google.com' ")
+    if not parse_args.url_link and not parse_args.url_csv:
+        print("Please enter a url link or csv file you want to test. Example: 'docker run fireworksai:app --url_link http:/www.google.com' ")
         exit()
     
-    # Check if URL is valid
-    if not validators.url(parse_args.url_link):
-        print(f"{parse_args.url_link} not valid. Please check link and be sure to include full https://... url.")
-        exit()
-    
-    main(parse_args.url_link,parse_args.n,parse_args.c,parse_args.qps)
-    
+    if parse_args.url_link:
+        # Check if URL is valid
+        if not validators.url(parse_args.url_link):
+            print(f"{parse_args.url_link} not valid. Please check link and be sure to include full https://... url.")
+            exit()
+        main(parse_args.url_link,parse_args.n,parse_args.c,parse_args.qps)
+    elif parse_args.url_csv:
+        main(parse_args.url_csv,parse_args.n,parse_args.c,parse_args.qps)
